c4d79d5490efdb77a1a74e21c779031be12023d0
Frankie <frankievx@gmail.com>
2019-07-10 13:27:26 -0400

    Linter fixes

--- packages/tiptap-utils/src/utils/markIsActive.js
+++ packages/tiptap-utils/src/utils/markIsActive.js
@@ -1,3 +1,23 @@
+function compareDeep(a, b) {
+  if (a === b) return true
+  if (!(a && typeof a === 'object')
+    || !(b && typeof b === 'object')) return false
+  const array = Array.isArray(a)
+  if (Array.isArray(b) !== array) return false
+  if (array) {
+    if (a.length !== b.length) return false
+    for (let i = 0; i < a.length; i += 1) if (!compareDeep(a[i], b[i])) return false
+  } else {
+    const keysA = Object.keys(a)
+    for (let i = 0; i < keysA.length; i += 1) {
+      if (!(keysA[i] in b) || !compareDeep(a[keysA[i]], b[keysA[i]])) return false
+    }
+    const keysB = Object.keys(b)
+    for (let i = 0; i < keysA.length; i += 1) if (!(keysB[i] in a)) return false
+  }
+  return true
+}
+
 export default function (state, type, attrs) {
   const {
     from,
@@ -9,33 +29,14 @@ export default function (state, type, attrs) {
   let result = false
 
   if (empty) {
-    result =!!type.isInSet(state.storedMarks || $from.marks())
-  }else{
+    result = !!type.isInSet(state.storedMarks || $from.marks())
+  } else {
     result = !!state.doc.rangeHasMark(from, to, type)
   }
 
-  if(attrs && attrs != {} && result){
-    result = $from.marks().find(x=>compareDeep(x.attrs,attrs))
+  if (attrs && attrs !== {} && result) {
+    result = $from.marks().find(x => compareDeep(x.attrs, attrs))
   }
 
   return result
 }
-
-function compareDeep(a, b) {
-  if (a === b) return true
-  if (!(a && typeof a == "object") ||
-      !(b && typeof b == "object")) return false
-  let array = Array.isArray(a)
-  if (Array.isArray(b) != array) return false
-  if (array) {
-    if (a.length != b.length) return false
-    for (let i = 0; i < a.length; i++) if (!compareDeep(a[i], b[i])) return false
-  } else {
-    for (let p in a) if (!(p in b) || !compareDeep(a[p], b[p])) return false
-    for (let p in b) if (!(p in a)) return false
-  }
-  return true
-}
-
-
-
